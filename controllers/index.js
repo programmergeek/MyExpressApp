77af9f68f64c11091725f5097cc8a4f2f37a551e
programmergeek <programergeek@gmail.com>
2020-09-10 15:10:20 +0200

    landing page getting started and first form

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,5 @@
-exports.index = function(req, res, next) {
-    res.render('index', { title: 'Express' });
+exports.get_landing = function(req, res, next) {
+    res.render('landing', { title: 'Express' });
   }
 
   //These allow us to separate the routes and the logic from each other
